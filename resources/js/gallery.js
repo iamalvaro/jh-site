956f4568148a035930730d08a8c7809ba05100b1
Alvaro Fernandez <alvaro.fdezalvarez@gmail.com>
2023-04-08 11:35:49 +0200

    gallery tab system

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -16,6 +16,26 @@ window.addEventListener("resize", function() {
   
 })
 
+//Gallery tab system
+function openPage(pageName,elmnt,color,fontColor) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+  elmnt.style.color = fontColor;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 // COPYRIGHT DYNAMIC YEAR
 
 const year = new Date().getFullYear();
