13e7d8303780893036f257a9fe5291a03670210c
Alvaro Fernandez <alvaro.fdezalvarez@gmail.com>
2023-04-11 11:39:25 +0200

    gallery content added

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -36,6 +36,30 @@ function openPage(pageName,elmnt,color,fontColor) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+//GALLERY-MODAL LOGIC
+
+ // Get the modal
+ var modal = document.getElementById("myModal");
+
+ // Get the image and insert it inside the modal - use its "alt" text as a caption
+ var img = document.querySelectorAll(".gallery-img");
+ 
+ var modalImg = document.getElementById("img01");
+ var captionText = document.getElementById("caption");
+ img.forEach(e => e.onclick = function(){
+   modal.style.display = "block";
+   modalImg.src = this.src;
+   captionText.innerHTML = this.alt;
+ });
+ 
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+ 
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function() { 
+   modal.style.display = "none";
+ }
+
 // COPYRIGHT DYNAMIC YEAR
 
 const year = new Date().getFullYear();
