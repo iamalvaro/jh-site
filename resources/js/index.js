952e5e5070941a4d4827fd01bccb541d561cae28
Alvaro Fernandez <alvaro.fdezalvarez@gmail.com>
2023-04-12 10:54:37 +0200

    modal onclick added to slideshow

--- resources/js/index.js
+++ resources/js/index.js
@@ -129,27 +129,27 @@ function showSlides(n) {
 }
 
 // // GALLERY-MODAL LOGIC
-//   // Get the modal
-// var modal = document.getElementById("myModal");
+  // Get the modal
+var modal = document.getElementById("myModal");
 
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.querySelectorAll(".gallery-img");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelectorAll(".slide-img");
 
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.forEach(e => e.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// });
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.forEach(e => e.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+});
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() { 
-//   modal.style.display = "none";
-// }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 
 // COPYRIGHT DYNAMIC YEAR
 const year = new Date().getFullYear();
