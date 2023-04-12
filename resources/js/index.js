a5aed24983552c5ffbfbcc1d280928b59d2f6175
Alvaro Fernandez <alvaro.fdezalvarez@gmail.com>
2023-04-12 10:46:22 +0200

    slide show created

--- resources/js/index.js
+++ resources/js/index.js
@@ -97,28 +97,59 @@ function initComparisons() {
 
   initComparisons();
 
-// GALLERY-MODAL LOGIC
-  // Get the modal
-var modal = document.getElementById("myModal");
+// SLIDE-SHOW
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.querySelectorAll(".gallery-img");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.forEach(e => e.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-});
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// // GALLERY-MODAL LOGIC
+//   // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.querySelectorAll(".gallery-img");
+
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.forEach(e => e.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// });
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
 
 // COPYRIGHT DYNAMIC YEAR
 const year = new Date().getFullYear();
